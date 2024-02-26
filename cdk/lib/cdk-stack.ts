acc083f66064ed53617925c2097acaf79f8a1fbd
nirgluzman <nir.gluzman@gmail.com>
2024-02-26 13:06:19 +0100

    add Lambda with sample hello

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,8 +1,15 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
 
 export class CdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    // define an AWS Lambda resource
+    const hello = new Function(this, 'HelloHandler', {
+      runtime: Runtime.NODEJS_16_X, // execution environment
+      code: Code.fromAsset('lambda'), // code loaded from "lambda" folder
+      handler: 'hello.handler' // file is "hello", function is "handler"
+    });
   }
 }
