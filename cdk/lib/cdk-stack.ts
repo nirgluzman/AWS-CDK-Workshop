2660ad418190d38fcf43f25ffd1ac5cb0ed00364
nirgluzman <nir.gluzman@gmail.com>
2024-02-26 13:52:41 +0100

    add API Gateway construct

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,15 +1,22 @@
 import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
+import path = require('path');
 
 export class CdkStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // define an AWS Lambda resource
+    // define an AWS Lambda resource.
     const hello = new Function(this, 'HelloHandler', {
       runtime: Runtime.NODEJS_16_X, // execution environment
-      code: Code.fromAsset('lambda'), // code loaded from "lambda" folder
+      code: Code.fromAsset(path.join(__dirname, '..', 'lambda')), // code loaded from "lambda" folder
       handler: 'hello.handler' // file is "hello", function is "handler"
+    });
+
+    // define an API Gateway REST API resource backed by "hello" function, using Lambda proxy integration.
+    new LambdaRestApi(this, 'Endpoint', {
+      handler: hello
     });
   }
 }
